86b00dd091c217013f56d14d9f0d451f83a522c8
kerem-alba <c.kerem.albayrak@gmail.com>
2024-03-11 23:12:13 +0300

    Added Database Tables and Migrations

---
+++ Data/LeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public int LeaveTypeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public string EmployeeId { get; set; }
+        public LeaveType LeaveType { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
