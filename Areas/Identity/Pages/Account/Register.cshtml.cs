5191a1fb3c7b7c74e157ebb0356f93e27aea1f9d
kerem-alba <c.kerem.albayrak@gmail.com>
2024-04-19 22:30:39 +0300

    Fixed register to automatic confirmation

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,32 +108,49 @@ namespace LeaveManagement.Web.Areas.Identity.Pages.Account
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
+                //if (result.Succeeded)
+                //{
+                //    _logger.LogInformation("User created a new account with password.");
+                //
+                //    _userManager.AddToRoleAsync(user, Roles.User).Wait();
+                //    var userId = await _userManager.GetUserIdAsync(user);
+                //    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                //    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                //    var callbackUrl = Url.Page(
+                //        "/Account/ConfirmEmail",
+                //        pageHandler: null,
+                //        values: new { area = "Identity", userId, code, returnUrl },
+                //        protocol: Request.Scheme);
+                //
+                //    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                //        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                //
+                //    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                //    {
+                //        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                //    }
+                //    else
+                //    {
+                //        await _signInManager.SignInAsync(user, isPersistent: false);
+                //        return LocalRedirect(returnUrl);
+                //    }
+                //}
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
-                    _userManager.AddToRoleAsync(user, Roles.User).Wait();
-                    var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId, code, returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    var confirmResult = await _userManager.ConfirmEmailAsync(user, code);
+                    if (!confirmResult.Succeeded)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                        throw new InvalidOperationException("Error confirming email.");
                     }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
+
+                    // Add user to the default role
+                    await _userManager.AddToRoleAsync(user, Roles.User);
+
+                    // Optionally skip confirmation and sign in the user
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
                 {
--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -1,26 +1,26 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using System.Net.Mail;
 
 namespace LeaveManagement.Web.Services
 {
     public class EmailSender : IEmailSender
     {
-        private readonly string _smtpServer;
-        private readonly int _smtpPort;
-        private readonly string _fromEmail;
+        private string smtpServer;
+        private int smtpPort;
+        private string fromEmailAddress;
 
-        public EmailSender(string smtpServer, int smtpPort, string fromEmail)
+        public EmailSender(string smtpServer, int smtpPort, string fromEmailAddress)
         {
-            _smtpServer = smtpServer;
-            _smtpPort = smtpPort;
-            _fromEmail = fromEmail;
+            this.smtpServer = smtpServer;
+            this.smtpPort = smtpPort;
+            this.fromEmailAddress = fromEmailAddress;
         }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var message = new MailMessage()
+            var message = new MailMessage
             {
-                From = new MailAddress(_fromEmail),
+                From = new MailAddress(fromEmailAddress),
                 Subject = subject,
                 Body = htmlMessage,
                 IsBodyHtml = true
@@ -28,12 +28,10 @@ namespace LeaveManagement.Web.Services
 
             message.To.Add(new MailAddress(email));
 
-            using (var client = new SmtpClient(_smtpServer, _smtpPort))
-            {
-                client.Send(message);
-            }
+            using var client = new SmtpClient(smtpServer, smtpPort);
+            client.Send(message);
+
             return Task.CompletedTask;
-            
         }
     }
 }
